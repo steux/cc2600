70f231edaa5b1f10d30163f9adc5c6c090359bce
Bruno STEUX <B.STEUX@nexter-group.fr>
2022-11-23 11:05:06 +0100

    refactored code generator

--- src/generate.rs
+++ src/generate.rs
@@ -18,13 +18,27 @@ impl<'a> GeneratorState<'a> {
         self.file.write(s.as_bytes())
     }
     fn write_asm(&mut self, asm: &str, cycles: u32) -> Result<usize, std::io::Error> {
-        self.file.write(format!("\t{}\t; {} cycles\n", asm, cycles).as_bytes())
+        self.file.write(format!("\t{:23}\t; {} cycles\n", asm, cycles).as_bytes())
     }
 }
 
-enum Hint {
-    MoveToX,
-    MoveToY,
+enum ExprType<'a> {
+    Immediate(i32),
+    Absolute(&'a str),
+    AbsoluteX(&'a str),
+    AbsoluteY(&'a str),
+    A, X, Y,
+}
+
+enum FlagsState {
+    Unknown,
+    A, X, Y,
+    Zero, Positive, Negative 
+}
+
+struct ExprOutput<'a> {
+    t: ExprType<'a>,
+    zero: FlagsState,
 }
 
 fn generate_included_source_code_line<'a>(loc: usize, gstate: &'a mut GeneratorState) -> Option<&'a str>
@@ -60,41 +74,182 @@ fn generate_included_source_code_line<'a>(loc: usize, gstate: &'a mut GeneratorS
 }
 
 
-fn generate_assign(lhs: &Expr, rhs: &Expr, state: &State, gstate: &mut GeneratorState, pos: usize) -> Result<(), Error>
+fn generate_assign<'a>(lhs: &Expr, rhs: &Expr<'a>, state: &State<'a>, gstate: &mut GeneratorState, pos: usize) -> Result<ExprOutput<'a>, Error>
 {
     match lhs {
         Expr::Var((var, sub)) => {
             match *var {
                 "X" => {
-                    generate_expr(rhs, state, gstate, pos, Some(Hint::MoveToX))?;
+                    let expr_output = generate_expr(rhs, state, gstate, pos)?;
+                    match expr_output.t {
+                        ExprType::Immediate(v) => {
+                            gstate.write_asm(&format!("LDX #{}", v), 2)?;
+                            Ok(ExprOutput { t: ExprType::X, zero: if v > 0 { FlagsState::Positive } else if v < 0 { FlagsState::Negative } else { FlagsState::Zero }})
+                        },
+                        ExprType::Absolute(name) => {
+                            let v = state.get_variable(name);
+                            gstate.write_asm(&format!("LDX {}", name), if v.zeropage {3} else {4})?;
+                            Ok(ExprOutput { t: ExprType::X, zero: FlagsState::X })
+                        },
+                        ExprType::AbsoluteX(name) => {
+                            gstate.write_asm(&format!("LDA {},X", name), 4)?;
+                            gstate.write_asm(&"TAX", 2)?;
+                            Ok(ExprOutput { t: ExprType::X, zero: FlagsState::X })
+                        },
+                        ExprType::AbsoluteY(name) => {
+                            gstate.write_asm(&format!("LDX {},Y", name), 4)?;
+                            Ok(ExprOutput { t: ExprType::X, zero: FlagsState::X })
+                        },
+                        ExprType::A => {
+                            gstate.write_asm(&"TAX", 2)?;
+                            Ok(ExprOutput { t: ExprType::X, zero: FlagsState::X })
+                        },
+                        ExprType::X => {
+                            Ok(ExprOutput { t: ExprType::X, zero: FlagsState::X })
+                        },
+                        ExprType::Y => {
+                            gstate.write_asm(&"TYA", 2)?;
+                            gstate.write_asm(&"TAX", 2)?;
+                            Ok(ExprOutput { t: ExprType::X, zero: FlagsState::X })
+                        },
+                    }
                 },
                 "Y" => {
-                    generate_expr(rhs, state, gstate, pos, Some(Hint::MoveToY))?;
+                    let expr_output = generate_expr(rhs, state, gstate, pos)?;
+                    match expr_output.t {
+                        ExprType::Immediate(v) => {
+                            gstate.write_asm(&format!("LDY #{}", v), 2)?;
+                            Ok(ExprOutput { t: ExprType::Y, zero: if v > 0 { FlagsState::Positive } else if v < 0 { FlagsState::Negative } else { FlagsState::Zero }})
+                        },
+                        ExprType::Absolute(name) => {
+                            let v = state.get_variable(name);
+                            gstate.write_asm(&format!("LDY {}", name), if v.zeropage {3} else {4})?;
+                            Ok(ExprOutput { t: ExprType::Y, zero: FlagsState::Y })
+                        },
+                        ExprType::AbsoluteX(name) => {
+                            gstate.write_asm(&format!("LDY {},X", name), 4)?;
+                            Ok(ExprOutput { t: ExprType::Y, zero: FlagsState::Y })
+                        },
+                        ExprType::AbsoluteY(name) => {
+                            gstate.write_asm(&format!("LDA {},X", name), 4)?;
+                            gstate.write_asm(&"TAY", 2)?;
+                            Ok(ExprOutput { t: ExprType::Y, zero: FlagsState::Y })
+                        },
+                        ExprType::A => {
+                            gstate.write_asm(&"TAY", 2)?;
+                            Ok(ExprOutput { t: ExprType::Y, zero: FlagsState::Y })
+                        },
+                        ExprType::X => {
+                            gstate.write_asm(&"TXA", 2)?;
+                            gstate.write_asm(&"TAY", 2)?;
+                            Ok(ExprOutput { t: ExprType::Y, zero: FlagsState::Y })
+                        },
+                        ExprType::Y => {
+                            Ok(ExprOutput { t: ExprType::Y, zero: FlagsState::Y })
+                        },
+                    }
                 },
                 variable => {
-                    generate_expr(rhs, state, gstate, pos, None)?;
                     let v = state.get_variable(variable);
                     let cycles = if v.zeropage { 3 } else { 4 };
-                    gstate.write_asm(&format!("STA {}", variable), cycles)?;
+                    let expr_output = generate_expr(rhs, state, gstate, pos)?;
+                    match expr_output.t {
+                        ExprType::Immediate(v) => {
+                            gstate.write_asm(&format!("LDA #{}", v), 2)?;
+                            match sub {
+                                Subscript::None => gstate.write_asm(&format!("STA {}", variable), cycles)?,
+                                Subscript::X => gstate.write_asm(&format!("STA {},X", variable), cycles + 1)?,
+                                Subscript::Y => gstate.write_asm(&format!("STA {},Y", variable), 5)?,
+                            };
+                            Ok(ExprOutput { t: ExprType::A, zero: if v > 0 { FlagsState::Positive } else if v < 0 { FlagsState::Negative } else { FlagsState::Zero }})
+                        },
+                        ExprType::Absolute(name) => {
+                            let v = state.get_variable(name);
+                            gstate.write_asm(&format!("LDA {}", name), if v.zeropage {3} else {4})?;
+                            match sub {
+                                Subscript::None => gstate.write_asm(&format!("STA {}", variable), cycles)?,
+                                Subscript::X => gstate.write_asm(&format!("STA {},X", variable), cycles + 1)?,
+                                Subscript::Y => gstate.write_asm(&format!("STA {},Y", variable), 5)?,
+                            };
+                            Ok(ExprOutput { t: ExprType::A, zero: FlagsState::A })
+                        },
+                        ExprType::AbsoluteX(name) => {
+                            gstate.write_asm(&format!("LDA {},X", name), 4)?;
+                            match sub {
+                                Subscript::None => gstate.write_asm(&format!("STA {}", variable), cycles)?,
+                                Subscript::X => gstate.write_asm(&format!("STA {},X", variable), cycles + 1)?,
+                                Subscript::Y => gstate.write_asm(&format!("STA {},Y", variable), 5)?,
+                            };
+                            Ok(ExprOutput { t: ExprType::A, zero: FlagsState::A })
+                        },
+                        ExprType::AbsoluteY(name) => {
+                            gstate.write_asm(&format!("LDA {},Y", name), 4)?;
+                            match sub {
+                                Subscript::None => gstate.write_asm(&format!("STA {}", variable), cycles)?,
+                                Subscript::X => gstate.write_asm(&format!("STA {},X", variable), cycles + 1)?,
+                                Subscript::Y => gstate.write_asm(&format!("STA {},Y", variable), 5)?,
+                            };
+                            Ok(ExprOutput { t: ExprType::A, zero: FlagsState::A })
+                        },
+                        ExprType::A => {
+                            match sub {
+                                Subscript::None => gstate.write_asm(&format!("STA {}", variable), cycles)?,
+                                Subscript::X => gstate.write_asm(&format!("STA {},X", variable), cycles + 1)?,
+                                Subscript::Y => gstate.write_asm(&format!("STA {},Y", variable), 5)?,
+                            };
+                            Ok(ExprOutput { t: ExprType::A, zero: expr_output.zero })
+                        },
+                        ExprType::X => {
+                            match sub {
+                                Subscript::None => gstate.write_asm(&format!("STX {}", variable), cycles)?,
+                                Subscript::X => {
+                                    gstate.write_asm(&"TXA", 2)?;
+                                    gstate.write_asm(&format!("STA {},X", variable), cycles + 1)?
+                                },
+                                Subscript::Y => if v.zeropage {
+                                    gstate.write_asm(&format!("STX {},Y", variable), 4)?
+                                } else {
+                                    gstate.write_asm(&"TXA", 2)?;
+                                    gstate.write_asm(&format!("STA {},Y", variable), 5)?
+                                },
+                            };
+                            Ok(ExprOutput { t: ExprType::X, zero: expr_output.zero })
+                        },
+                        ExprType::Y => {
+                            match sub {
+                                Subscript::None => gstate.write_asm(&format!("STY {}", variable), cycles)?,
+                                Subscript::X => if v.zeropage {
+                                    gstate.write_asm(&format!("STY {},X", variable), 4)?
+                                } else {
+                                    gstate.write_asm(&"TYA", 2)?;
+                                    gstate.write_asm(&format!("STA {},X", variable), 5)?
+                                },
+                                Subscript::Y => {
+                                    gstate.write_asm(&"TYA", 2)?;
+                                    gstate.write_asm(&format!("STA {},Y", variable), 5)?
+                                },
+                            };
+                            Ok(ExprOutput { t: ExprType::Y, zero: expr_output.zero })
+                        },
+                    }
                 }
             }
         },
-        _ => return Err(syntax_error(state, "Bad left value in assignement", pos)),
+        _ => Err(syntax_error(state, "Bad left value in assignement", pos)),
     }
-    Ok(())
 }
 
-fn generate_expr(expr: &Expr, state: &State, gstate: &mut GeneratorState, pos: usize, hint: Option<Hint>) -> Result<(), Error>
+fn generate_expr<'a>(expr: &Expr<'a>, state: &State<'a>, gstate: &mut GeneratorState, pos: usize) -> Result<ExprOutput<'a>, Error>
 {
     // Include C source code into generated asm
-    debug!("{:?}, {}, {}, {}", expr, pos, gstate.last_included_position, gstate.last_included_line_number);
+    // debug!("{:?}, {}, {}, {}", expr, pos, gstate.last_included_position, gstate.last_included_line_number);
     let included_source_code = generate_included_source_code_line(pos, gstate);
     let line_to_be_written = if let Some(line) = included_source_code {
         Some(line.to_string())
     } else {
         None
     };
-    debug!("{:?}, {}, {}", line_to_be_written, gstate.last_included_position, gstate.last_included_line_number);
+    // debug!("{:?}, {}, {}", line_to_be_written, gstate.last_included_position, gstate.last_included_line_number);
     if let Some(l) = line_to_be_written {
         let f = &mut gstate.file; 
         f.write_all(b";")?;
@@ -103,81 +258,38 @@ fn generate_expr(expr: &Expr, state: &State, gstate: &mut GeneratorState, pos: u
 
     debug!("Expression: {:?}", expr);
     match expr {
-        Expr::Integer(i) => {
-            match hint {
-                None => gstate.write_asm(&format!("LDA {}", i), 2)?,
-                Some(Hint::MoveToX) => gstate.write_asm(&format!("LDX {}", i), 2)?,
-                Some(Hint::MoveToY) => gstate.write_asm(&format!("LDY {}", i), 2)?,
-            };
-        },
+        Expr::Integer(i) => Ok(ExprOutput { t: ExprType::Immediate(*i), zero: FlagsState::Unknown }),
         Expr::BinOp {lhs, op, rhs} => {
             match op {
-                Operation::Assign => {
-                    generate_assign(lhs, rhs, state, gstate, pos)?;
-                },
-                Operation::Add => return Err(Error::Unimplemented { feature: "Addition not implemented" }),
-                Operation::Sub => return Err(Error::Unimplemented { feature: "Subtraction not implemented" }),
-                Operation::Eq => return Err(Error::Unimplemented { feature: "Equal not implemented" }),
-                Operation::Neq => return Err(Error::Unimplemented { feature: "Not equal not implemented" }),
-                Operation::Gt => return Err(Error::Unimplemented { feature: "Comparison not implemented" }),
-                Operation::Gte => return Err(Error::Unimplemented { feature: "Comparison not implemented" }),
-                Operation::Lt => return Err(Error::Unimplemented { feature: "Comparison not implemented" }),
-                Operation::Lte => return Err(Error::Unimplemented { feature: "Comparison not implemented" }),
-            };
+                Operation::Assign => generate_assign(lhs, rhs, state, gstate, pos),
+                Operation::Add => Err(Error::Unimplemented { feature: "Addition not implemented" }),
+                Operation::Sub => Err(Error::Unimplemented { feature: "Subtraction not implemented" }),
+                Operation::Eq => Err(Error::Unimplemented { feature: "Equal not implemented" }),
+                Operation::Neq => Err(Error::Unimplemented { feature: "Not equal not implemented" }),
+                Operation::Gt => Err(Error::Unimplemented { feature: "Comparison not implemented" }),
+                Operation::Gte => Err(Error::Unimplemented { feature: "Comparison not implemented" }),
+                Operation::Lt => Err(Error::Unimplemented { feature: "Comparison not implemented" }),
+                Operation::Lte => Err(Error::Unimplemented { feature: "Comparison not implemented" }),
+            }
         },
         Expr::Var((var, sub)) => {
             match *var {
-                "X" => {
-                    match hint {
-                        Some(Hint::MoveToX) => { },
-                        Some(Hint::MoveToY) => {
-                            gstate.write_asm("TXA", 2)?;
-                            gstate.write_asm("TAY", 2)?;
-                        },
-                        None => {
-                            gstate.write_asm("TXA", 2)?;
-                        }
-                    }
-                },
-                "Y" => {
-                    match hint {
-                        Some(Hint::MoveToX) => {
-                            gstate.write_asm("TYA", 2)?;
-                            gstate.write_asm("TAX", 2)?;
-                        },
-                        Some(Hint::MoveToY) => {
-                        },
-                        None => {
-                            gstate.write_asm("TYA", 2)?;
-                        }
-                    }
-                },
-                variable => {
-                    let v = state.get_variable(variable);
-                    let cycles = if v.zeropage { 3 } else { 4 };
-                    match hint {
-                        Some(Hint::MoveToX) => {
-                            gstate.write_asm(&format!("LDX {}", variable), cycles)?;
-                        },
-                        Some(Hint::MoveToY) => {
-                            gstate.write_asm(&format!("LDY {}", variable), cycles)?;
-                        },
-                        None => {
-                            gstate.write_asm(&format!("LDA {}", variable), cycles)?;
-                        }
-                    }
+                "X" => Ok(ExprOutput { t: ExprType::X, zero: FlagsState::Unknown }),
+                "Y" => Ok(ExprOutput { t: ExprType::Y, zero: FlagsState::Unknown }),
+                variable => match sub {
+                    Subscript::None => Ok(ExprOutput { t: ExprType::Absolute(variable), zero: FlagsState::Unknown }),
+                    Subscript::X => Ok(ExprOutput { t: ExprType::AbsoluteX(variable), zero: FlagsState::Unknown }),
+                    Subscript::Y => Ok(ExprOutput { t: ExprType::AbsoluteY(variable), zero: FlagsState::Unknown }),
                 }
             }
         },
         _ => unreachable!() 
     }
-
-    Ok(())
 }
 
 fn generate_for_loop(init: &Expr, condition: &Expr, update: &Expr, body: &StatementLoc, state: &State, gstate: &mut GeneratorState, pos: usize) -> Result<(), Error>
 {
-    generate_expr(init, state, gstate, pos, None)?;
+    generate_expr(init, state, gstate, pos)?;
     Err(Error::Unimplemented { feature: "for loop statement not implemented" })
 }
 
@@ -191,7 +303,7 @@ fn generate_statement(code: &StatementLoc, state: &State, gstate: &mut Generator
             }
             Ok(())
         },
-        Statement::Expression(expr) => generate_expr(&expr, state, gstate, code.pos, None),
+        Statement::Expression(expr) => { generate_expr(&expr, state, gstate, code.pos)?; Ok(()) },
         Statement::For { init, condition, update, body } => generate_for_loop(init, condition, update, body.as_ref(), state, gstate, code.pos),
         Statement::If { condition, body, else_body } => Err(Error::Unimplemented { feature: "if statement not implemented" }),
         Statement::Break => Err(Error::Unimplemented { feature: "break statement not implemented" }),
