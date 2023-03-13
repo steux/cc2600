72f16f0e7261c617adad06c49aef19be34019778
Bruno STEUX <B.STEUX@nexter-group.fr>
2023-03-13 11:45:31 +0100

    added stdint.h header

---
+++ headers/stdint.h
@@ -0,0 +1,12 @@
+// This file defines simple standard types 
+// It is distributed as a companion to cc2600.
+
+#ifndef __STDINT_H__
+#define __STDINT_H__
+
+#define int8_t signed char
+#define uint8_t unsigned char
+#define int16_t signed short
+#define uint16_t unsigned short
+
+#endif // __STDINT_H__
