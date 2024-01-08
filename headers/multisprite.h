74b0c647970e27dc18cff2e0796486331102ec25
Bruno STEUX <bruno.steux@gmail.com>
2024-01-08 12:29:48 +0100

    making progress on multisprite header

--- headers/multisprite.h
+++ headers/multisprite.h
@@ -12,7 +12,6 @@
 #define MAX_NB_SPRITES 10
 #endif
 
-char ms_next_mode;
 char ms_v, ms_colup0, ms_colup1;
 char *ms_colup0ptr, *ms_colup1ptr, *ms_grp0ptr, *ms_grp1ptr, *ms_scenery;
 char ms_next_slice;
@@ -23,7 +22,7 @@ char ms_sprite_y[MAX_NB_SPRITES];
 char ms_sprite_id[MAX_NB_SPRITES];
 char ms_nusiz[MAX_NB_SPRITES];
 char ms_sorted_by_y[MAX_NB_SPRITES];
-char ms_tmp, ms_tmp2;
+char ms_tmp;
 char ms_id_p[2];
 char ms_nb_sprites;
 
@@ -32,10 +31,10 @@ const char ms_sprite_hm[153] = {0x70, 0x60, 0x50, 0x40, 0x30, 0x70, 0x60, 0x50, 
 
 const char sprite1[1] = {0};
 
-#define NB_SPRITES_DEF 2
-const char *ms_grptr[NB_SPRITES_DEF] = {sprite1, 0};
-const char *ms_coluptr[NB_SPRITES_DEF] = {sprite1, 0};
-const char ms_height[NB_SPRITES_DEF] = {8, 0};
+#define NB_SPRITES_DEF 1
+const char *ms_grptr[NB_SPRITES_DEF] = {sprite1};
+const char *ms_coluptr[NB_SPRITES_DEF] = {sprite1};
+const char ms_height[NB_SPRITES_DEF] = {8};
 
 void ms_select_sprites()
 {
@@ -80,201 +79,192 @@ inline char ms_allocate_sprite()
 void kernel()
 {
     ms_select_sprites();
+    
     // Phase 1: before the kernel actually starts, allocates and positions sprites p0 and p1.
     ms_sprite_iter = 0;
+    *GRP0 = 0;
+    *VDELP0 = 0;
     ms_id_p[0] = ms_allocate_sprite(); // 47
     X = ms_id_p[0];
-    // Position sprite 1
+    // Position sprite 0
     if (X != -1) {
+        Y = ms_sprite_id[X];
+        ms_tmp = ms_sprite_y[X];
+        ms_grp0ptr = ms_grptr[Y] - ms_tmp;   // 21
+        ms_colup0ptr = ms_coluptr[Y] - ms_tmp; // 21
         strobe(WSYNC);                  // 3
-        X = ms_sprite_x[X];             // 7
+        
+        X = ms_sprite_x[X];             // 6
         X = ms_sprite_wait[X];          // 6
         *HMP0 = ms_sprite_hm[X];        // 7
+        do { X--; } while (X);          // 5 / cycle. 4 for the last one. 
+        strobe(RESP0);                  // 3. Minimum = 26 cycles
+        strobe(WSYNC);                  // 3
+        strobe(HMOVE);
+    }
+    *GRP1 = 0;
+    ms_id_p[1] = ms_allocate_sprite(); // 47
+    X = ms_id_p[1];
+    // Position sprite 1
+    if (X != -1) {
+        Y = ms_sprite_id[X];
+        ms_tmp = ms_sprite_y[X];
+        ms_grp1ptr = ms_grptr[Y] - ms_tmp;   // 21
+        ms_colup1ptr = ms_coluptr[Y] - ms_tmp; // 21
+        strobe(WSYNC);                  // 3
+        
+        X = ms_sprite_x[X];             // 6
+        X = ms_sprite_wait[X];          // 6
+        *HMP0 = ms_sprite_hm[X];        // 7
+        do { X--; } while (X);          // 5 / cycle. 4 for the last one. 
+        strobe(RESP1);                  // 3. Minimum = 26 cycles
+        strobe(WSYNC);                  // 3
+        strobe(HMOVE);
+    }
+
+    // We are ready for the actual kernel
+    ms_next_slice = 192;
+    X = ms_id_p[0];
+    if (X != -1) {
+        ms_tmp = ms_sprite_y[X];
+        if (ms_tmp < ms_next_slice) ms_next_slice = ms_tmp;
+    }
+    Y = 0;
+    
+    // Void kernel
+    ms_v = ms_scenery[Y];               // 9
+    Y++;                                // 2
+    X = ms_scenery[Y];                  // 8
+    strobe(WSYNC);                      // 3
+    VSYNC[X] = ms_v;                    // 7
+    Y++;                                // 2
+    do {
+        kernel_long_macro;              // Macro max 76 * 2 - 40 = 112 cycles (min 76 - 9 = 67 cycles)
+        ms_v = ms_scenery[Y];           // 9
+        Y++;                            // 2
+        X = ms_scenery[Y];              // 8
+        strobe(WSYNC);                  // 3
+
+        VSYNC[X] = ms_v;                // 7
+        Y++;                            // 2
+    } while (Y != ms_next_slice);       // 6
+
+    // Now we are going into display 0 and repo 1 kernel
+    X = ms_id_p[1];
+    if (X != -1) {
+        // Display sprite 0 while preparing for next sprite 1
+        ms_v = ms_scenery[Y];           // 9
+        strobe(WSYNC);                  // 3
+
+        *GRP0 = ms_grp0ptr[Y];          // 9
+        *COLUP0 = ms_colup0ptr[Y];      // 9 
+        Y++;                            // 2
+                                        // Retrieve ms_x for next sprite
+        X = ms_sprite_iter;             // 3
+        X = ms_sorted_by_y[X];          // 6
+        ms_tmp = X;                     // 3. To be used later
+        ms_x = ms_sprite_x[X];          // 7
+        X = ms_scenery[Y];              // 8
+        *COLUP0 = ms_colup0ptr[Y];      // 9 // This color change is anticipateed. Color artifact when sprite 0 is on the right of the screen
+        load(ms_grp0ptr[Y]);            // 6
+        Y++;                            // 2
+        strobe(WSYNC);                  // 3. Total (2) = 67
 
         store(*GRP0);                   // 3
         VSYNC[X] = ms_v;                // 7
                                         // Player 1 repositionning
         X = ms_x;                       // 3
         X = ms_sprite_wait[X];          // 6
-        if (X) {                        // 3
-            do { X--; } while (X);      // 5 / cycle. 4 for the last one. 
-        }
-        strobe(RESP0);                  // 3. Minimum = 24 cycles (ideally 28, so left part of the screen is not reachable)
-        strobe(HMOVE);
+        do { X--; } while (X);          // 5 / cycle. 4 for the last one. 
+        strobe(RESP1);                  // 3. Minimum = 26 cycles
+        strobe(WSYNC);                  // 3. Total (3) = Unknown
+
+        *GRP0 = ms_grp0ptr[Y];          // 9
+        *COLUP0 = ms_colup0ptr[Y];      // 9 
+        ms_v = ms_scenery[Y];           // 9
+        Y++;                            // 2
+                                        // Setup new NUSIZ for new sprite 
+        X = ms_tmp;                     // 3
+        *NUSIZ1 = ms_nusiz[X];          // 7
+        X = ms_x;                       // 3
+        *HMP1 = ms_sprite_hm[X];        // 7
+        X = ms_scenery[Y];              // 8
+        ms_colup0 = ms_colup0ptr[Y];    // 9 
+        load(ms_grp0ptr[Y]);            // 6
+        strobe(WSYNC);                  // 3. Total (4) = 75 cycles
+
+        store(*GRP0);                   // 3
+        *COLUP0 = ms_colup0;            // 6 / 9 
+        VSYNC[X] = ms_v;                // 7 / 16
+                                        // Wait for 73 - 16 cycles exactly
+        X = ms_tmp;                     // 3 / 19 
+        X = ms_sprite_id[X];            // 6 / 25 
+        ms_tmp = Y;                     // 3 / 28
+        ms_grp1ptr = ms_grptr[X] - ms_tmp;   // 21 / 39
+        ms_colup1ptr = ms_coluptr[X] - ms_tmp; // 21 / 71
+        strobe(HMOVE);                  // 3. Early HMOVE at cycle 74
+                                        //strobe(WSYNC);                // Total (5) = 74
+
+        Y++;                            // 2 (for free) 
+        *GRP0 = ms_grp0ptr[Y];          // 9
+        *COLUP0 = ms_colup0ptr[Y];      // 9 
+        ms_v = ms_scenery[Y];           // 9
+        Y++;                            // 2
+        X = ms_scenery[Y];              // 8
+        strobe(WSYNC);                  // 3. Total (6) = 40 cycles
+
+        *GRP0 = ms_grp0ptr[Y];          // 9
+        VSYNC[X] = ms_v;                // 7
+        *COLUP0 = ms_colup0ptr[Y];      // 9  // This color change is slightly delayed. Color artifact when sprite 0 is on the left of the screen
+        Y++;                            // 2
     }
-    ms_id_p[1] = ms_allocate_sprite(); // 47
+
+    // Simply display sprite p0
+    do {
+        ms_v = ms_scenery[Y];       // 9
+        strobe(WSYNC);              // 3
+
+        *GRP0 = ms_grp0ptr[Y];      // 9
+        *COLUP0 = ms_colup0ptr[Y];  // 9 
+        kernel_macro; // Max 15 cycles
+        Y++;                        // 2
+        X = ms_scenery[Y];          // 8
+        load(ms_grp0ptr[Y]);        // 6
+        strobe(WSYNC);              // 3
+
+        store(*GRP0);               // 3
+        *COLUP0 = ms_colup0ptr[Y];  // 9 
+        VSYNC[X] = ms_v;            // 7
+        Y++;                        // 2
+    } while (Y != ms_next_slice);   // 6
+
+    // And then let's go into display 0 & 1 kernel
+    *VDELP0 = 1;                        // 5
+    do {
+        ms_v = ms_scenery[Y];           // 9
+        ms_colup0 = ms_colup0ptr[Y];    // 9
+        ms_colup1 = ms_colup1ptr[Y];    // 9
+        *GRP0 = ms_grp0ptr[Y];          // 9
+        load(ms_grp1ptr[Y]);            // 6
+        strobe(WSYNC);                  // 3 ; Total (1) = 29 + 36 + 9 = 75
+
+        store(*GRP1);                   // 3
+        *COLUP0 = ms_colup0;            // 6 
+        *COLUP1 = ms_colup1;            // 6
+        kernel_macro;                   // Max 15 cycles
+        Y++;                            // 2
+        X = ms_scenery[Y];              // 8
+        ms_colup0 = ms_colup0ptr[Y];    // 9
+        ms_colup1 = ms_colup1ptr[Y];    // 9
+        *GRP0 = ms_grp0ptr[Y];          // 9
+        load(ms_grp1ptr[Y]);            // 6
+        strobe(WSYNC);                  // 3: Total (2) = 61 
+
+        store(*GRP1);                   // 3
+        *COLUP0 = ms_colup0;            // 6
+        *COLUP1 = ms_colup1;            // 6
+        VSYNC[X] = ms_v;                // 7
+        Y++;                            // 2
+    } while (Y != ms_next_slice);       // 6
+    *VDELP0 = 0;                        // 5
 }
-
-void try_kernel()
-{
-kernel_start:
-    if (ms_next_mode & 1) {
-        if (ms_next_mode & 2) {
-            *VDELP0 = 1;                        // 5
-            // 2 sprites display
-            do {
-                ms_v = ms_scenery[Y];           // 9
-                ms_colup0 = ms_colup0ptr[Y];    // 9
-                ms_colup1 = ms_colup1ptr[Y];    // 9
-                *GRP0 = ms_grp0ptr[Y];          // 9
-                load(ms_grp1ptr[Y]);            // 6
-                strobe(WSYNC);                  // 3 ; Total (1) = 29 + 36 + 9 = 75
-                
-                store(*GRP1);                   // 3
-                *COLUP0 = ms_colup0;            // 6 
-                *COLUP1 = ms_colup1;            // 6
-                kernel_macro;                   // Max 15 cycles
-                Y++;                            // 2
-                X = ms_scenery[Y];              // 8
-                ms_colup0 = ms_colup0ptr[Y];    // 9
-                ms_colup1 = ms_colup1ptr[Y];    // 9
-                *GRP0 = ms_grp0ptr[Y];          // 9
-                load(ms_grp1ptr[Y]);            // 6
-                strobe(WSYNC);                  // 3: Total (2) = 61 
-                
-                store(*GRP1);                   // 3
-                *COLUP0 = ms_colup0;            // 6
-                *COLUP1 = ms_colup1;            // 6
-                VSYNC[X] = ms_v;                // 7
-                Y++;                            // 2
-            } while (Y != ms_next_slice);       // 6
-            *VDELP0 = 0;                        // 5
-            goto kernel_start;                  // 5
-            // We are 37 cycles inside this line
-        } else {
-            // No sprite display kernel
-            // Prepare for next pair of sprites
-            ms_v = ms_scenery[Y];               // 9
-            Y++;                                // 2
-            X = ms_scenery[Y];                  // 8
-            strobe(WSYNC);                      // 3
-            VSYNC[X] = ms_v;                    // 7
-            Y++;                                // 2
-            do {
-                kernel_long_macro;              // Macro max 76 * 2 - 40 = 112 cycles (min 76 - 9 = 67 cycles)
-                 ms_v = ms_scenery[Y];          // 9
-                Y++;                            // 2
-                X = ms_scenery[Y];              // 8
-                strobe(WSYNC);                  // 3
-
-                VSYNC[X] = ms_v;                // 7
-                Y++;                            // 2
-            } while (Y != ms_next_slice);       // 6
-            goto kernel_start;                  // 3
-        }
-    } else {
-        if (ms_next_mode & 2) {
-            // Sprite 0 mode only
-            // Display sprite 0 while preparing for next sprite 1
-            // We are 37 + 8 + 7 = 52 cycles inside this line
-            *GRP1 = 0;                      // 5
-            ms_v = ms_scenery[Y];           // 9
-            strobe(WSYNC);                  // 3. Total (1) = 52 + 9 + 8 = 69           
-
-            *GRP0 = ms_grp0ptr[Y];          // 9
-            *COLUP0 = ms_colup0ptr[Y];      // 9 
-            Y++;                            // 2
-            // Retrieve ms_x for next sprite
-            X = ms_sprite_iter;             // 3
-            X = ms_sorted_by_y[X];          // 6
-            ms_tmp = X;                     // 3. To be used later
-            ms_x = ms_sprite_x[X];          // 7
-            X = ms_scenery[Y];              // 8
-            *COLUP0 = ms_colup0ptr[Y];      // 9 // This color change is anticipateed. Color artifact when sprite 0 is on the right of the screen
-            load(ms_grp0ptr[Y]);            // 6
-            Y++;                            // 2
-            strobe(WSYNC);                  // 3. Total (2) = 67
-            
-            store(*GRP0);                   // 3
-            VSYNC[X] = ms_v;                // 7
-            // Player 1 repositionning
-            X = ms_x;                       // 3
-            X = ms_sprite_wait[X];          // 6
-            if (X) {                        // 3
-                do { X--; } while (X);      // 5 / cycle. 4 for the last one. 
-            }
-            strobe(RESP1);                  // 3. Minimum = 24 cycles (ideally 28, so left part of the screen is not reachable)
-            strobe(WSYNC);                  // 3. Total (3) = Unknown
-            
-            *GRP0 = ms_grp0ptr[Y];          // 9
-            *COLUP0 = ms_colup0ptr[Y];      // 9 
-            ms_v = ms_scenery[Y];           // 9
-            Y++;                            // 2
-            // Setup new NUSIZ for new sprite 
-            X = ms_tmp;                     // 3
-            *NUSIZ1 = ms_nusiz[X];          // 7
-            X = ms_x;                       // 3
-            *HMP1 = ms_sprite_hm[X];        // 7
-            X = ms_scenery[Y];              // 8
-            ms_colup0 = ms_colup0ptr[Y];    // 9 
-            load(ms_grp0ptr[Y]);            // 6
-            strobe(WSYNC);                  // 3. Total (4) = 75 cycles
-
-            store(*GRP0);                   // 3
-            *COLUP0 = ms_colup0;            // 6 / 9 
-            VSYNC[X] = ms_v;                // 7 / 16
-            // Wait for 73 - 16 cycles exactly
-            X = ms_tmp;                     // 3 / 19 
-            X = ms_sprite_id[X];            // 6 / 25 
-            ms_tmp = Y;                     // 3 / 28
-            ms_grp1ptr = ms_grptr[X] - ms_tmp;   // 21 / 39
-            ms_colup1ptr = ms_coluptr[X] - ms_tmp; // 21 / 71
-            strobe(HMOVE);                  // 3. Early HMOVE at cycle 74
-            //strobe(WSYNC);                // Total (5) = 74
-            
-            Y++;                            // 2 (for free) 
-            *GRP0 = ms_grp0ptr[Y];          // 9
-            *COLUP0 = ms_colup0ptr[Y];      // 9 
-            ms_v = ms_scenery[Y];           // 9
-            Y++;                            // 2
-            X = ms_scenery[Y];              // 8
-            strobe(WSYNC);                  // 3. Total (6) = 40 cycles
-            
-            *GRP0 = ms_grp0ptr[Y];          // 9
-            VSYNC[X] = ms_v;                // 7
-            *COLUP0 = ms_colup0ptr[Y];      // 9  // This color change is slightly delayed. Color artifact when sprite 0 is on the left of the screen
-            Y++;                            // 2
-            
-            do {
-                ms_v = ms_scenery[Y];       // 9
-                strobe(WSYNC);              // 3
-                
-                *GRP0 = ms_grp0ptr[Y];      // 9
-                *COLUP0 = ms_colup0ptr[Y];  // 9 
-                kernel_macro; // Max 15 cycles
-                Y++;                        // 2
-                X = ms_scenery[Y];          // 8
-                load(ms_grp0ptr[Y]);        // 6
-                strobe(WSYNC);              // 3
-                
-                store(*GRP0);               // 3
-                *COLUP0 = ms_colup0ptr[Y];  // 9 
-                VSYNC[X] = ms_v;            // 7
-                Y++;                        // 2
-            } while (Y != ms_next_slice);   // 6
-            goto kernel_start;              // 3
-        } else {
-            // Sprite 1 mode only
-            // Display sprite 1 while preparing for next sprite 1
-            // We are 32 + 8 + 8 = 48 cycles inside this line
-            do {
-                ms_v = ms_scenery[Y];           // 9
-                strobe(WSYNC);                  // 3 
-                
-                *GRP1 = ms_grp1ptr[Y];          // 9
-                *COLUP1 = ms_colup1ptr[Y];      // 9 
-                kernel_macro;                   // Max 15 cycles
-                Y++;                            // 2
-                X = ms_scenery[Y];              // 8
-                strobe(WSYNC);                  // 3 
-                
-                *GRP1 = ms_grp1ptr[Y];          // 9
-                *COLUP1 = ms_colup1ptr[Y];      // 9 
-                VSYNC[X] = ms_v;                // 7
-                Y++;                            // 2
-            } while (Y != ms_next_slice);       // 3
-            goto kernel_start;                  // 3
-        }
-    }
-}
-
